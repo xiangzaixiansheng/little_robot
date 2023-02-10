a7ab49a3d41457f43047648b52c9d320503b53d1
hanxiang1 <hanxiang1@staff.sina.com.cn>
2023-02-10 17:12:14 +0800

    perf:优化文件

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -1,11 +1,13 @@
 package util
 
 import (
-	"github.com/sirupsen/logrus"
+	"io"
 	"log"
 	"os"
 	"path"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 var LogrusObj *logrus.Logger
@@ -19,20 +21,25 @@ func init() {
 	}
 	//实例化
 	logger := logrus.New()
-	src, _ := setOutputFile()
-	//设置输出
-	logger.Out = src
+	writer1_file, _ := setOutputFile() //文件
+	writer2_console := os.Stdout       //终端
+
+	//同时写入终端和文件中
+	logger.SetOutput(io.MultiWriter(writer1_file, writer2_console))
 	//设置日志级别
 	logger.SetLevel(logrus.DebugLevel)
 	//设置日志格式
-	logger.SetFormatter(&logrus.TextFormatter{
+	logger.SetFormatter(&logrus.JSONFormatter{
 		TimestampFormat: "2006-01-02 15:04:05",
 	})
+	//增加行号和文件名
+	logger.SetReportCaller(true)
+
 	/*
-	加个hook形成ELK体系
-	但是考虑到一些同学一下子接受不了那么多技术栈，
-	所以这里的ELK体系加了注释，如果想引入可以直接注释去掉，
-	如果不想引入这样注释掉也是没问题的。
+		加个hook形成ELK体系
+		但是考虑到一些同学一下子接受不了那么多技术栈，
+		所以这里的ELK体系加了注释，如果想引入可以直接注释去掉，
+		如果不想引入这样注释掉也是没问题的。
 	*/
 	//hook := model.EsHookLog()
 	//logger.AddHook(hook)
@@ -70,3 +77,17 @@ func setOutputFile() (*os.File, error) {
 	return src, nil
 }
 
+/*
+    log.Trace("Something very low level.")
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+	// Calls os.Exit(1) after logging
+	//log.Fatal("Bye.")
+	// Calls panic() after logging
+	log.Panic("I'm bailing.")
+	log.WithFields(log.Fields{
+		"animal": "dog", //增加字段打印
+	}).Info("dog is here")
+**/
